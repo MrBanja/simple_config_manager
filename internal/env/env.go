b72bef87040f561bcf6385a9514f61bff76a3602
AngliD <mcrus.com@gmail.com>
2023-05-21 14:15:24 +0400

    feat: Make vars case-insensitive.

--- internal/env/env.go
+++ internal/env/env.go
@@ -8,17 +8,18 @@ import (
 )
 
 func Get(envPrefix string, errorOnEmpty bool) (map[string]string, error) {
+	envPrefix = strings.ToUpper(envPrefix)
 	vars := make(map[string]string)
 
 	for _, env := range os.Environ() {
 		s := strings.SplitN(env, "=", 2)
 		name, value := s[0], s[1]
-
+		name = strings.ToUpper(name)
 		if strings.HasPrefix(name, envPrefix) {
-			name = strings.ToUpper(strings.TrimPrefix(name, envPrefix))
 			if value == "" && errorOnEmpty {
 				return nil, errors.New(fmt.Sprintf("env [%s] value is empty", name))
 			}
+			name = strings.TrimPrefix(name, envPrefix)
 			vars[name] = value
 		}
 	}
